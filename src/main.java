69d8a3ce4b566908002dba02971604decf80f12c
AKrelov <wsup@bk.ru>
2022-11-16 19:30:19 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+
+        int amount = 16000;
+        int bonus = 20;
+
+
+        System.out.println((amount / bonus) + "Бонусных миль");
+
+    }
+}
